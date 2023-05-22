f6f3475fa1b0ddd190fd89d2f273953dfcaf6175
khalil <khalillakhdharatc@gmail.com>
2023-05-22 16:09:54 +0100

    update

--- src/pages/Reactpage.js
+++ src/pages/Reactpage.js
@@ -1,9 +1,38 @@
 import React from 'react'
-
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
 function Reactpage() {
-  return (
-    <div>Reactpage</div>
-  )
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(response => {
+    setData(response.data);
+    })
+    .catch(error => {
+    console.log(error);
+    });
+    }, []);
+    return (
+    <div>
+    <h1>Liste des taches</h1>
+    <table className="table table-bordered">
+    <thead>
+    <tr>
+    <th>title</th><th>Completed</th>
+</tr>
+</thead>
+<tbody>
+{data.map(user => (
+<tr key={user.id}>
+<td>{user.title}</td>
+<td>{user.completed.toString()}</td>
+</tr>
+))}
+</tbody>
+</table>
+</div>
+);
 }
+
 
 export default Reactpage
